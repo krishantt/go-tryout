55ef688353d2e43ad2e95b8e2afecdc7c10b8558
krishtimil <krishtimil@gmail.com>
2024-06-05 22:32:07 +0545

    fix client binary in grpc

--- grpc/client/bin/qotd.go
+++ grpc/client/bin/qotd.go
@@ -5,3 +5,22 @@ import (
     "flag"
 	"fmt"
 	"github.com/krishantt/go-tryout/grpc/client"
+)
+
+var(
+	addr = flag.String("addr", "127.0.0.1:80", "The address to connect to.")
+	author = flag.String("author", "", "The author whoose quote to get")
+)
+
+func main() {
+	flag.Parse()
+    c, err := client.New(*addr)
+    if err!= nil {
+        panic(err)
+    }
+    author, quote, err := c.QOTD(context.Background(), *author)
+    if err!= nil {
+        panic(err)
+    }
+    fmt.Println(author, quote)
+}
